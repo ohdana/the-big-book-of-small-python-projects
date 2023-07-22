5b24c65325d9b7b57fee2e2b15d95d844e98e768
Dana Goosen <klymentovska@gmail.com>
2023-07-22 12:24:58 +0200

    fixed bugs after dev testing

--- day5_caesar_cipher/caesarcipher.py
+++ day5_caesar_cipher/caesarcipher.py
@@ -10,41 +10,55 @@ def main():
         play()
 
 def play():
-    action = input(STRINGS_DICTIONARY.encrypt_or_decrypt)
-
-    while not is_valid_action(action):
-        action = input(STRINGS_DICTIONARY.invalid_encrypt_or_decrypt)
+    action = get_action()
 
     if action == 'e':
         trigger_action_encrypt()
     elif action == 'd':
         trigger_action_decrypt()
 
-    print(STRINGS_DICTIONARY.separator)
+    show_footer()
 
 def trigger_action_encrypt():
-    user_message = input(STRINGS_DICTIONARY.enter_message_encrypt)
-
-    key = input(STRINGS_DICTIONARY.enter_the_key)
-    while not is_valid_key(key):
-        key = input(STRINGS_DICTIONARY.enter_the_key)
-
-    encrypted_message = encrypt(user_message.lower(), int(key))
-    print(STRINGS_DICTIONARY.your_encrypted_message.format(encrypted_message))
+    message = get_message(STRINGS_DICTIONARY.enter_message_encrypt)
+    key = get_key()
+    encrypted_message = encrypt(message.lower(), key)
+    show_result(STRINGS_DICTIONARY.your_encrypted_message, encrypted_message)
 
 def trigger_action_decrypt():
-    user_message = input(STRINGS_DICTIONARY.enter_message_decrypt)
+    message = get_message(STRINGS_DICTIONARY.enter_message_decrypt)
+    key = get_key()
+    decrypted_message = decrypt(message.lower(), key)
+    show_result(STRINGS_DICTIONARY.your_decrypted_message, decrypted_message)
 
+def get_message(prompt_text):
+    message = input(prompt_text)
+
+    while not message:
+        message = input(STRINGS_DICTIONARY.invalid_message)
+
+    return message
+
+def get_action():
+    action = input(STRINGS_DICTIONARY.encrypt_or_decrypt)
+
+    while not is_valid_action(action):
+        action = input(STRINGS_DICTIONARY.invalid_encrypt_or_decrypt)
+
+    return action
+
+def get_key():
     key = input(STRINGS_DICTIONARY.enter_the_key)
-    decrypted_message = decrypt(user_message.lower(), int(key))
 
     while not is_valid_key(key):
         key = input(STRINGS_DICTIONARY.enter_the_key)
-    print(STRINGS_DICTIONARY.your_decrypted_message.format(decrypted_message))
+
+    return int(key)
 
 def is_valid_key(key):
     if not key.isdigit():
         return False
+
     return MIN_KEY <= int(key) <= MAX_KEY
 
 def is_valid_action(action):
@@ -65,12 +79,21 @@ def get_shifted_char(char, key):
 
     char_ord_in_abc = ABC.index(char)
     new_char_ord_in_abc = char_ord_in_abc + key
+    print(new_char_ord_in_abc)
+
     if new_char_ord_in_abc < 0:
-        new_char_ord_in_abc = len(ABC) - new_char_ord_in_abc
-    elif new_char_ord_in_abc > len(ABC):
+        new_char_ord_in_abc += len(ABC)
+    elif new_char_ord_in_abc >= len(ABC):
         new_char_ord_in_abc -= len(ABC)
 
     return chr(new_char_ord_in_abc + ord('a'))
+
+def show_result(pattern, message):
+    print(pattern.format(message))
+
+def show_footer():
+    print(STRINGS_DICTIONARY.separator)
+    print(STRINGS_DICTIONARY.lets_play_again)
 
 def show_intro_message():
     print(STRINGS_DICTIONARY.intro_message)
@@ -107,6 +130,11 @@ def init_strings_dictionary():
     Your decrypted message: {}'''
     STRINGS_DICTIONARY.separator = '''
     ************************************************'''
+    STRINGS_DICTIONARY.lets_play_again = '''
+    Let's play again!
+    '''
+    STRINGS_DICTIONARY.invalid_message = '''
+    Message cannot be empty. Please enter the message: '''
 
 class StringsDictionary:
     pass
