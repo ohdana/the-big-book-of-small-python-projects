50999a3f0716cd3577bc01c2704b54d35c3a4721
Dana Goosen <klymentovska@gmail.com>
2023-08-16 22:28:05 +0200

    fixes after devtesting

--- day29_flooder/flooder.py
+++ day29_flooder/flooder.py
@@ -1,33 +1,96 @@
-YES, NO, QUIT = 'Y', 'N', 'QUIT'
-HEART, TRIANGLE, DIAMOND, BALL, CLUB, SPADE = 'H', 'T', 'D', 'B', 'C', 'S'
-GAME_OVER = False
-CANVAS_WIDTH = 20
-CANVAS_HEIGHT = 20
+from frame import Frame
+
+HEART, TRIANGLE, DIAMOND, BALL, CLUB, SPADE, CROSS = 'H', 'T', 'D', 'B', 'C', 'S', 'R'
+CANVAS_WIDTH, CANVAS_HEIGHT = 10, 10
+CURSOR_X, CURSOR_Y = 1, 1
 INITIAL_N_OF_MOVES = 13
+
+HEART_CHAR      = chr(9829)  # Character 9829 is '♥'
+DIAMOND_CHAR    = chr(9830)  # Character 9830 is '♦'
+SPADE_CHAR      = chr(9824)  # Character 9824 is '♠'
+CLUB_CHAR       = chr(9827)  # Character 9827 is '♣'
+BALL_CHAR       = chr(9679)  # Character 9679 is '●'
+TRIANGLE_CHAR   = chr(9650)  # Character 9650 is '▲'
+CROSS_CHAR   = '+'
+
+YES, NO, QUIT = 'Y', 'N', 'QUIT'
+EASY, MEDIUM, HARD = 'E', 'M', 'H'
+DIFFICULTY_TYPES = {
+    EASY: [HEART_CHAR, BALL_CHAR, CROSS_CHAR],
+    MEDIUM: [HEART_CHAR, BALL_CHAR, TRIANGLE_CHAR, BALL_CHAR, CROSS_CHAR],
+    HARD: [HEART_CHAR, DIAMOND_CHAR, SPADE_CHAR, CLUB_CHAR, BALL_CHAR, TRIANGLE_CHAR, CROSS_CHAR] }
+DIFFICULTY_NAMES_MAP = {}
+CHARS_MAP = { HEART: HEART_CHAR, DIAMOND: DIAMOND_CHAR, SPADE: SPADE_CHAR, CLUB: CLUB_CHAR,
+    BALL: BALL_CHAR, TRIANGLE: TRIANGLE_CHAR, CROSS: CROSS_CHAR}
 
 def main():
     init()
     show_intro_message()
-    play()
+    play_again = True
+    while play_again:
+        play()
+        play_again = prompt_play_again()
+    say_bye()
 
 def play():
-    user_input = get_user_input()
-    while not GAME_OVER:
-        pass
+    moves_left = INITIAL_N_OF_MOVES
+    difficulty = get_difficulty_level()
+    available_chars = DIFFICULTY_TYPES[difficulty]
+    frame = Frame(CANVAS_WIDTH, CANVAS_HEIGHT, available_chars)
+    while True:
+        print(STRINGS_DICTIONARY.moves_left.format(moves_left))
+        frame.show_canvas()
+        user_input = get_user_input()
+        moves_left = make_move(frame, user_input, moves_left)
+        if moves_left == 0 or frame.is_filled_up_with_one_char():
+            break
 
-    say_bye()
+    frame.show_canvas()
+    show_results(frame, difficulty, INITIAL_N_OF_MOVES - moves_left)
+
+def make_move(frame, user_input, moves_left):
+    new_char = CHARS_MAP[user_input]
+    old_char = frame.get()[CURSOR_Y][CURSOR_X]
+    cells_to_update = frame.get_matching_neighbours(old_char, CURSOR_X, CURSOR_Y)
+    cells_to_update.append((CURSOR_X, CURSOR_Y))
+    update_cells(new_char, frame, cells_to_update)
+
+    return moves_left - 1
+
+def update_cells(char, frame, cells):
+    for cell in cells:
+        x, y = cell
+        frame.update_cell(x, y, char)
+
+def show_results(frame, difficulty, moves_used):
+    print(STRINGS_DICTIONARY.difficulty.format(DIFFICULTY_NAMES_MAP[difficulty]))
+    print(STRINGS_DICTIONARY.moves_used.format(moves_used, INITIAL_N_OF_MOVES))
+    if player_won(frame):
+        print(STRINGS_DICTIONARY.you_won)
+    else:
+        print(STRINGS_DICTIONARY.you_lost)
+
+def player_won(frame):
+    return frame.is_filled_up_with_one_char()
+
+def get_difficulty_level():
+    user_input = input(STRINGS_DICTIONARY.choose_difficulty_level)
+    if user_input.upper() not in [EASY, MEDIUM, HARD]:
+        return get_difficulty_level()
+
+    return user_input.upper()
 
 def get_user_input():
     global GAME_OVER
     user_input = input(STRINGS_DICTIONARY.choose_one)
-   if user_input.upper() == QUIT:
+    if user_input.upper() == QUIT:
         GAME_OVER = True
         return
 
-    if user_input.upper() not in [HEART, TRIANGLE, DIAMOND, BALL, CLUB, SPADE]:
+    if user_input.upper() not in [HEART, TRIANGLE, DIAMOND, BALL, CLUB, SPADE, CROSS]:
         return get_user_input()
 
-    return user_input
+    return user_input.upper()
 
 def prompt_play_again():
     answer = ''
@@ -41,10 +104,7 @@ def is_valid_y_n(answer):
     if not answer:
         return False
 
-    return answer in [YES, NO]
-
-def reset_canvas():
-    pass
+    return answer.upper() in [YES, NO]
 
 def say_bye():
     print(STRINGS_DICTIONARY.bye)
@@ -52,8 +112,14 @@ def say_bye():
 def show_intro_message():
     print(STRINGS_DICTIONARY.intro_message)
 
+def init_difficulty_names_map():
+    DIFFICULTY_NAMES_MAP[EASY] = STRINGS_DICTIONARY.easy
+    DIFFICULTY_NAMES_MAP[MEDIUM] = STRINGS_DICTIONARY.medium
+    DIFFICULTY_NAMES_MAP[HARD] = STRINGS_DICTIONARY.hard
+
 def init():
     init_strings_dictionary()
+    init_difficulty_names_map()
 
 ##############################
 def init_strings_dictionary():
@@ -66,22 +132,32 @@ def init_strings_dictionary():
     Idea by Al Sweigart al@inventwithpython.com
     Implementation by @ohdana
 
-    A colourful game where you try to fill the board with a single colour. Has
-    a mode for colourblind players.
-    Set the upper left colour/shape, which fills in all the adjacent squares of that colour/shape. Try to make the
-    entire board the same colour/shape.'''
+    A colourful game where you try to fill the board with a single shape.
+    Set the upper left colour/shape, which fills in all the adjacent squares of that shape. Try to make the
+    entire board the same shape.'''
     STRINGS_DICTIONARY.bye = '''
     Bye!'''
     STRINGS_DICTIONARY.colour_mode = '''
     Do you want to play in colourblind mode? y/n: '''
     STRINGS_DICTIONARY.moves_left = '''
-    Moves left: '''
+    Moves left: {}'''
     STRINGS_DICTIONARY.you_won = '''
     You won!'''
-    STRINGS_DICTIONARY.no_moves_left = '''
-    You ran out of moves.'''
+    STRINGS_DICTIONARY.you_lost = '''
+    You lost.'''
     STRINGS_DICTIONARY.choose_one = '''
-    Choose one of (H)eart, (T)riangle, (D)iamond, (B)all, (C)lub, (S)pade or QUIT: '''
+    Choose one of (H)eart, (T)riangle, (D)iamond, (B)all, (C)lub, (S)pade, c(R)oss or QUIT: '''
+    STRINGS_DICTIONARY.choose_difficulty_level = '''
+    Choose the difficulty -- (E)asy, (M)edium, (H)ard: '''
+    STRINGS_DICTIONARY.difficulty = '''
+    Difficulty level: {}'''
+    STRINGS_DICTIONARY.moves_used = '''
+    Moves used: {} out of {}'''
+    STRINGS_DICTIONARY.play_again = '''
+    Play again? y/n: '''
+    STRINGS_DICTIONARY.easy = 'EASY'
+    STRINGS_DICTIONARY.medium = 'MEDIUM'
+    STRINGS_DICTIONARY.hard = 'HARD'
 
 class StringsDictionary:
     pass
--- day29_flooder/frame.py
+++ day29_flooder/frame.py
@@ -1,3 +1,5 @@
+import random
+
 CURSOR_CHAR = '>'
 FRAME_TOP_LEFT_CHAR = chr(9484)  # Character 9484 is '┌'
 FRAME_BOTTOM_LEFT_CHAR = chr(9492)  # Character 9492 is '└'
@@ -7,16 +9,24 @@ FRAME_HORIZONTAL_CHAR = chr(9472)  # Character 9472 is '─'
 FRAME_VARTICAL_CHAR = chr(9474)  # Character 9474 is '│'
 
 class Frame:
-    def __init__(self, width, height):
+    def __init__(self, width, height, available_chars):
+        self.available_chars = available_chars
         self.width = width
         self.height = height
-        self.canvas = self.get_canvas()
+        self.canvas = self.get_new_canvas()
+        self.fill_up()
 
-    def get_canvas(self):
+    def get(self):
+        return self.canvas
+
+    def get_new_canvas(self):
         frame = self.build_frame()
         canvas = self.add_cursor(frame)
 
         return canvas
+
+    def update_cell(self, x, y, char):
+        self.canvas[y][x] = char
 
     def build_frame(self):
         canvas = []
@@ -44,9 +54,47 @@ class Frame:
 
         return canvas
 
+    def get_matching_neighbours(self, char, x, y, cells=None):
+        if not cells:
+            cells = []
+        left = (x - 1, y)
+        right = (x + 1, y)
+        top = (x, y - 1)
+        bottom = (x, y + 1)
+        within_bounds = lambda x, y: 0 < x <= self.width and 0 < y <= self.height
+        neighbours = [neighbour for neighbour in [left, top, right, bottom] if within_bounds(*neighbour)]
+
+        for neighbour in neighbours:
+            neighbour_x, neighbour_y = neighbour
+            if self.canvas[neighbour_y][neighbour_x] == char:
+                if not neighbour in cells:
+                    cells.append(neighbour)
+                    cells += self.get_matching_neighbours(char, neighbour_x, neighbour_y, cells)
+                cells.append(neighbour)
+
+        return list(set(cells))
+
+    def is_filled_up_with_one_char(self):
+        char = self.canvas[1][1]
+        for i in range(1, self.height + 1):
+            for j in range(1, self.width + 1):
+                if self.canvas[i][j] != char:
+                   return False
+
+        return True
+
     def show_canvas(self):
         lines = [''.join(line) for line in self.canvas]
         print('\n'.join(lines))
 
-frame = Frame(10, 10)
-frame.show_canvas()
+    def fill_up(self):
+        for i in range(1, self.height + 1):
+            for j in range(1, self.width + 1):
+                self.canvas[i][j] = self.get_random_char()
+
+    def get_random_char(self):
+        return random.choice(self.available_chars)
+
+#frame = Frame(10, 10, ['+', 'o', 'x'])
+#frame.update_cell(1, 1, 'h')
+#frame.show_canvas()
