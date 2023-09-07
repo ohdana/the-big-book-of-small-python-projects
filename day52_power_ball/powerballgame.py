1086cfcb0f1aa1d732090b2bf565480a096848a1
Dana Goosen <klymentovska@gmail.com>
2023-09-07 17:01:03 +0200

    remove unused file

--- day52_power_ball/powerballgame.py
+++
@@ -1,9 +0,0 @@
-class PowerBallGame:
-    def __init__(self):
-        self.player_won = False
-
-    def start_game(self):
-        pass
-
-    def did_player_win(self):
-        return self.player_won
